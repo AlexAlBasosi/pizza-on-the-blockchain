362d8f83d26ce139b023e599dc08e62a6bd50924
Alexander Al Basosi <tazlex15@gmail.com>
2018-01-22 20:29:38 +0400

    Update application.py

--- application.py
+++ application.py
@@ -31,8 +31,7 @@ def index():
 
 @app.route("/factory")
 def factory():
-	r = requests.get('http://localhost:3000/api/ChangeOwner') 
-	return render_template('factory.html', title="Factory", transactions=r.json())
+	return render_template('factory.html', title="Factory")
 
 @app.route("/submitPizza", methods=['POST', 'GET'])
 def submitPizza():
